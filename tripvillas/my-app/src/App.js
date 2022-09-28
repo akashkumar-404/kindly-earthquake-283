f5b1606b9eb5e0cee20c8595966fe07cf534ee43
maheshkumar-wd <maheshkumaryadav1430@gmail.com>
2022-09-28 20:05:55 +0530

    adding Homepage folder structure

--- tripvillas/my-app/src/App.js
+++ tripvillas/my-app/src/App.js
@@ -1,8 +1,13 @@
+import Footer from "./Homepage_tripVillas/Components/Footer/Footer";
+import Navbar from "./Homepage_tripVillas/Components/Navbar/Navbar";
+import HomePage from "./Homepage_tripVillas/HomePage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
---
+++ tripvillas/my-app/src/Homepage_tripVillas/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Navbar from "./Components/Navbar/Navbar";
+
+const HomePage = () => {
+     
+  return <div>
+    
+  </div>;
+};
+
+export default HomePage;
