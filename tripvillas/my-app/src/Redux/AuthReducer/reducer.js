1c5e3c7c4b2f18b52dca4a0d567d9e781c0aae93
Golla Mahesh Kumar <89634533+Maheshkumar-WD@users.noreply.github.com>
2022-09-28 14:27:40 +0530

    Update reducer.js

--- tripvillas/my-app/src/Redux/AuthReducer/reducer.js
+++ tripvillas/my-app/src/Redux/AuthReducer/reducer.js
@@ -4,7 +4,7 @@ const initialstate={
 export const reducer =(state=initialstate,action)=>{
     switch(action.type){
       // create your own case:
-        
+          
         default:
          return state
     }
