d0852893c75a82b0e9c9dd59a040829ee03289fe
Golla Mahesh Kumar <89634533+Maheshkumar-WD@users.noreply.github.com>
2022-09-28 11:43:41 +0530

    Creating boilercode for reducer.js in AppReducer

---
+++ tripvillas/my-app/src/Redux/AppReducer/reducer.js
@@ -0,0 +1,16 @@
+const reducer =(state,action)=>{
+  switch(action.type){
+    // create your own case:
+      
+      default:
+        state
+  }
+
+}
+
+
+// before pushing the code to repo 
+ // first we need to make pull request and then push code to repo
+ // 1. using `git pull origin main`
+  // 2. `git checkout <branch name>` and then
+ // using `git push origin <branch name>`
