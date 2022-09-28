6f9343d166e948f8d27f0a96a838dd678355f4f2
Golla Mahesh Kumar <89634533+Maheshkumar-WD@users.noreply.github.com>
2022-09-28 11:52:12 +0530

    making boilercode for actionTypes.js in AppReducer

---
+++ tripvillas/my-app/src/Redux/AppReducer/actionType.js
@@ -0,0 +1,3 @@
+// create your own actionType with naming convention like: 
+// IF YOU FIND ANY ACTION_TYPE THAT YOU WANT YOU CAN USE THAT INSTEAD OF CREATING NEW ACTION_TYPE
+// const GET_DATA_<your student_code> = "GET_DATA_<your student_code>"
