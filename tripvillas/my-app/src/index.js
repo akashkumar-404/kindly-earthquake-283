b39c1879b77a4c1337f84eb62da33be20321ba31
Arun Singh <aruns3435@gmail.com>
2022-10-02 15:39:17 +0530

    sign Up modal

--- tripvillas/my-app/src/index.js
+++ tripvillas/my-app/src/index.js
@@ -4,20 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // made imports from here
-// import { ChakraProvider } from "@chakra-ui/react";
-// import { store } from "./Redux/store";
-// import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <Provider store={store}>
-  //   <BrowserRouter>
-  //     <ChakraProvider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ChakraProvider>
         <App />
-  //     </ChakraProvider>
-  //   </BrowserRouter>
-  // </Provider>
+      </ChakraProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
